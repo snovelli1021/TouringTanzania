a01d846baaa2d253720c94e8033b05273e1c5c8e
Stephen Novelli <snovelli1021@gmail.com>
2023-07-22 16:13:44 -0700

    Changed PORT configuration to try and fix Heroku issue.

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-//Setting up server to run on PORT 3000.
+//Setting up server to run on PORT 8080.
 const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static("public"));
 
@@ -11,6 +11,6 @@ app.get("/", (req, res) => res.sendFile(path.join(__dirname, "./index.html")));
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || 8080, "0.0.0.0", () => {
   console.log(`Touring Tanzania listening at http://localhost:${PORT}`);
 });
